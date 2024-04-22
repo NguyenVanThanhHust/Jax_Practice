34cc4cee8d30b802602bd1f8324cc81edf25725c
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2024-04-22 22:50:09 +0700

    temp commit

---
+++ Autodidax/Readme.md
@@ -0,0 +1,1 @@
+https://jax.readthedocs.io/en/latest/autodidax.html
---
+++ Autodidax/autodidax.py
@@ -0,0 +1,16 @@
+import marimo
+
+__generated_with = "0.2.1"
+app = marimo.App()
+
+
+app._unparsable_cell(
+    r"""
+    import 
+    """,
+    name="__"
+)
+
+
+if __name__ == "__main__":
+    app.run()
